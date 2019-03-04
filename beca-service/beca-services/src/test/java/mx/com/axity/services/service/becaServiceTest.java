40d3c2e774ac071fc4363444f47671d3f004196c
EvannyR <evanny.rira@outlook.com>
2019-03-04 12:55:41 -0600

    beca service

---
+++ beca-service/beca-services/src/test/java/mx/com/axity/services/service/becaServiceTest.java
@@ -0,0 +1,34 @@
+package mx.com.axity.services.service;
+
+import mx.com.axity.commons.to.UserTO;
+import mx.com.axity.services.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+public class becaServiceTest extends BaseTest {
+
+    @Test
+    public void exampleTest() {
+
+        List<UserTO> users = this.becaService.getUsers();
+
+        Assert.assertEquals(1, users.size());
+    }
+    @Test
+    public void shouldReturnTheSumOfTwoNumbers(){
+        int sum1 = 5;
+        int sum2 = 10;
+        int result= becaService.sum(sum1,sum2);
+
+        Assert.assertEquals(15,result);
+    }
+    @Test
+    public void shouldReturnTheSubOfTwoNumbers(){
+        int sub1= 15;
+        int sub2= 5;
+        int result= becaService.sub(sub1,sub2);
+
+        Assert.assertEquals(10,result);
+    }
+}
